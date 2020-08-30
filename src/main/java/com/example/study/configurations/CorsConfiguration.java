eed3cdda874a7e6aaa571dbdb787f11114126a31
vinod.john <vinod.john@birkle-it.com>
2020-08-30 14:24:58 +0300

    Added Cors confuguration

---
+++ src/main/java/com/example/study/configurations/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.study.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration for CORS
+ *
+ * @author Vinod John
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT");
+    }
+}
